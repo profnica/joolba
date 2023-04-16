8f41835bfca4ab4005b425b40bd2439fa1c30d1e
Seun Fashina <fashinaoluwaseun36@gmail.com>
2023-04-15 18:02:08 -0700

    created enpoints for account verification and user registration

--- authentications/urls.py
+++ authentications/urls.py
@@ -1,23 +1,7 @@
-from django.urls import path, include
+from django.urls import path
 from .views import *
-from rest_framework_simplejwt.views import TokenRefreshView
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
-)
+
 urlpatterns = [
-    path("user", UserViewAPI.as_view({
-        'get': 'list',
-    }), name="user_list"),
-    path("user/<int:pk>", UserViewAPI.as_view({'get': 'retrieve', 'put': 'update',
-                                               'patch': 'partial_update',
-                                               'delete': 'destroy'}), name='user_api'),
-    path('login', MyObtainTokenPairView.as_view(), name='token_obtain_pair'),
-    path('token/refresh', TokenRefreshView.as_view(), name='token_refresh'),
-    path('logout', LogoutAndBlacklistRefreshToken.as_view(), name='blacklist_token'),
-    path('logout-all/', LogoutAllView.as_view(), name='auth_logout_all'),
-    path('change-password/<int:number>', ChangePasswordView.as_view(),
-         name='auth_change_password'),
-    path('register',
-         RegisterView.as_view({'post': 'create'}), name='auth_register'),
+    path('user/register', UserRegister.as_view(), name='register-user'),
+    path('account/verify/<str:uuidb64>/<str:token>', AccountVerification.as_view(), name='account_verification'),
 ]
