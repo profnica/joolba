c029768770bf6072cda143fa3495e46f50e2d928
Seun Fashina <fashinaoluwaseun36@gmail.com>
2023-04-15 18:01:06 -0700

    serialized the user model, validated email and password

--- authentications/serializers.py
+++ authentications/serializers.py
@@ -1,72 +1,42 @@
 from rest_framework import serializers
-# from .models import UserProfile
-from django.contrib.auth.models import User
-from rest_framework.validators import UniqueValidator
-from django.contrib.auth.password_validation import validate_password
-
-from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from django.contrib.auth import get_user_model
+from .validators import email_validator
+
+
 User = get_user_model()
 
-
-class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
-
-    @classmethod
-    def get_token(cls, user):
-        token = super(MyTokenObtainPairSerializer, cls).get_token(user)
-
-        # Add custom claims
-        token['email'] = user.email
-        token['phone_number'] = user.phone_number
-        return token
-
-# class UserProfileSerializer(serializers.ModelSerializer):
-#     class Meta:
-#         model = UserProfile
-#         fields = "__all__"
-
-
-class UserSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = ('id', 'password', 'email', 'phone_number',
-                  'first_name', 'last_name', 'created_at', 'updated_at')
-
-
-class RegisterSerializer(serializers.ModelSerializer):
-    email = serializers.EmailField(
-        required=True,
-        validators=[UniqueValidator(queryset=User.objects.all())]
-    )
-
-    password = serializers.CharField(
-        write_only=True, required=True, validators=[validate_password])
-    # password2 = serializers.CharField(write_only=True, required=True)
+class UserRegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, required=True)
+    confirm_password = serializers.CharField(write_only=True, required=True)
 
     class Meta:
         model = User
-        fields = ('password', 'email', 'first_name',
-                  'last_name', 'phone_number',)
-        extra_kwargs = {
-            'last_name': {'required': False}
-        }
+        fields = [
+            'id',
+            'name',
+            'email',
+            'password',
+            'confirm_password'
+        ]
 
-    def create(self, validated_data):
-        user = User.objects.create(
-            email=validated_data['email'],
-            first_name=validated_data['first_name'],
-            last_name=validated_data['last_name'],
-            phone_number=validated_data['phone_number'],
-        )
+    def validate_email(self,value):
+        """
+        check if the value is a valid email address
+        """
+        email_validator(value)
+        return value
 
-        user.set_password(validated_data['password'])
-        user.save()
+        
+    def validate(self, attrs):
+    
+        """
+        check if passwords match
+        """
+        password = attrs.get('password')
+        confirm_password = attrs.pop('confirm_password')
 
-        return user
-
-
-class ChangePasswordSerializer(serializers.Serializer):
-    model = User
-
-    old_password = serializers.CharField(required=True)
-    new_password = serializers.CharField(required=True)
+        if password != confirm_password:
+            raise serializers.ValidationError('passwords does not match')
+            
+        
+        return attrs
