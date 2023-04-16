93097f9b20703c71e93ad9e2ee21fe3d22ef41f6
Seun Fashina <fashinaoluwaseun36@gmail.com>
2023-04-16 15:59:05 -0700

    added jwt endpoints

--- authentications/urls.py
+++ authentications/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path
 from .views import *
+from rest_framework_simplejwt.views import TokenRefreshView
+
 
 urlpatterns = [
-    path('user/register', UserRegister.as_view(), name='register-user'),
+    path('login/', UserLogin.as_view(), name='token_obtain_pair'),
+    path('login/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('user/register', UserRegister.as_view(), name='register_user'),
     path('account/verify/<str:uuidb64>/<str:token>', AccountVerification.as_view(), name='account_verification'),
 ]
+
+# for the logout the client can simply discard the token and the user will be logged out
