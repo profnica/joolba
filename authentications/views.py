26a9241cc98373b7576d52b4f473e1c8e5e1a178
Seun Fashina <fashinaoluwaseun36@gmail.com>
2023-04-16 16:01:23 -0700

    refactored the views, added view to get access and refresh token

--- authentications/views.py
+++ authentications/views.py
@@ -1,45 +1,52 @@
 from django.db import IntegrityError
 from rest_framework import status
 from rest_framework.authtoken.models import Token
-from .serializers import UserRegisterSerializer
+from .serializers import UserRegisterSerializer, MyTokenObtainPairSerializer, UserSerializer
 from django.contrib.auth import get_user_model
 from rest_framework.generics import CreateAPIView, views
 from rest_framework.response import Response
-from .utils import send_verification_mail
+from .utils import send_verification_mail, create_token_for_user
+from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework.permissions import AllowAny
+
 
 User = get_user_model()
 
 
 class AccountVerification(views.APIView):
+    permission_classes = [AllowAny]
     def get(self, request):
         return Response({'message': 'Account verified!'})
 
+# create new user
 class UserRegister(CreateAPIView):
     serializer_class = UserRegisterSerializer
+    permission_classes = [AllowAny]
 
     def create(self, request, *args, **kwargs):
         serializer = self.get_serializer(data=request.data)
-        if serializer.is_valid():
-            user = serializer.save()
+        serializer.is_valid(raise_exception=True)
+        user = serializer.save()
 
-            # create tokens for each users that will be returned as a response
-            try:
-                token = Token.objects.create(user=user)
-            except IntegrityError:
-                token = Token.objects.get(user=user)
-                return Response({'error':'token must be unique for each user'}, status=status.HTTP_400_BAD_REQUEST)
+        # create tokens for each users that will be returned as a response
+        # token = create_token_for_user(user)
 
-            # get the domain name of the site
-            current_site = request.get_host()
+        # get the domain name of the site
+        current_site = request.get_host()
             
-            # send mail
-            send_verification_mail(current_site, user)
+        # send mail
+        send_verification_mail(current_site, user)
 
-            return Response({
-                'token':token.key,
-                'user': serializer.data,
-                'message':'sign up successful! A verification mail has been sent to your email, please click on the mail'
-                }, status=status.HTTP_201_CREATED)
-        else:
-            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        return Response({
+            # 'token':token.key, 
+            'user': UserSerializer(user, context={'request': request}).data,
+            'message':'Sign up successful! A verification mail has been sent to your email, please click on the mail'
+            }, status=status.HTTP_201_CREATED)
     
+    
+    
+
+# using jwt to handle token when user log in
+class UserLogin(TokenObtainPairView):
+    serializer_class = MyTokenObtainPairSerializer
+    permission_classes = [AllowAny]
